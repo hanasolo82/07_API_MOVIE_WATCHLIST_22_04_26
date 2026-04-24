bf202c9dfac707d321b931435bf866d2ea408f94
Hanasolo <hanasolo@hotmail.com>
2026-04-24 17:12:34 +0200

    geting data from api functions

--- src/main.js
+++ src/main.js
@@ -1,1 +1,33 @@
-("title-calification")
+const apiKey = import.meta.env.VITE_API_KEY 
+const baseAllUrl = `http://www.omdbapi.com/?apikey=${apiKey}&`
+const searchBtn = document.getElementById("input-btn")
+const formEl = document.getElementById("search-form")
+const inputEl = document.getElementById("input-search")
+
+
+
+
+
+
+formEl.addEventListener("submit", getFormValue)
+
+function getFormValue(e) {
+    e.preventDefault()
+    const inputValue = inputEl.value
+    
+    if(inputValue) {
+        async function dataApi() {
+            const response = await fetch(baseAllUrl + `s=${inputValue}`)
+            const data = await response.json()
+            console.log(data.Search.slice(0, 3))
+            //const shortData = data.slice(0, 1)
+           // localStorage.setItem("movies info", JSON.stringify(data))
+           // console.log(shortData)
+            }
+             dataApi() 
+    }
+    
+    
+    
+}
+
